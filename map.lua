a071871a368615ad4377290d35158a257e3a499b
Axhar <olivier.boucher.game@hotmail.com>
2023-08-17 21:44:05 -0400

    update

--- map.lua
+++ map.lua
@@ -1,4 +1,4 @@
-rednet.open("side")
+rednet.open("back")
 local layer = {y_max = 0, x_max = 0}
 local data_phones = {{0,{x = 0, y = 0, z = 0}}}
 local data_rooms = {{x = 0, y = 0, z = 0, autorized = nil, receiver_id = 0, name = "", lpos = {x = 0, y = 0, z = 0}}}
@@ -6,18 +6,17 @@ local data = {phones = data_phones, rooms = data_rooms}
 local map_len = 80
 local center_origin = {x = 130, y = 60, z = 684}
 local map_origin = {x = 130 + map_len/2, y = 60, z = 684 + map_len/2}
-local m = peripheral.wrap("top")
+local m = peripheral.wrap("right")
 local old_term = term.redirect(m)
 local width, height = m.getSize()
 local cal_x = width/(center_origin.x - (map_len/2))
 local cal_y = height/(center_origin.y - (map_len/2))
 m.setCursorPos(1, 1)
-m.setTextScale(5)
+m.setTextScale(1)
 
 function draw_area()
     for i = 1, #data.rooms do
-        paintutils.drawBox((data.rooms[i].lpos.x - map_origin.x)*cal_x, (data.rooms[i].lpos.y - map_origin.y)*cal_y, 
-        (data.rooms[i].x - map_origin.x)*cal_x, (data.rooms[i].y - map_origin.y)*cal_y, colors.green)
+        paintutils.drawBox(data.rooms[i].lpos.x - map_origin.x, data.rooms[i].lpos.z - map_origin.z, data.rooms[i].x - map_origin.x, data.rooms[i].z - map_origin.z, colors.green)
     end
 end
 
@@ -33,6 +32,7 @@ end
 
 function get_data()
     local sender_id, message, protocol_distance = rednet.receive("lights_dispacher_440")
+    print(sender_id)
     if (sender_id == 24) then
         data = {phones = message[1], rooms = message[2]}
     end
