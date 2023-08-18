838ddae1c38153ed0671f21e2969440964d0b819
Axhar <olivier.boucher.game@hotmail.com>
2023-08-18 10:48:21 -0400

    update

--- map.lua
+++ map.lua
@@ -1,20 +1,28 @@
-rednet.open("back")
-local layer = {y_max = 0, x_max = 0}
-local data_phones = {{0,{x = 0, y = 0, z = 0}}}
-local data_rooms = {{x = 0, y = 0, z = 0, autorized = nil, receiver_id = 0, name = "", lpos = {x = 0, y = 0, z = 0}}}
-local data = {phones = data_phones, rooms = data_rooms}
-local map_len = 80
-local center_origin = {x = 130, y = 60, z = 684}
-local map_origin = {x = 130 + map_len/2, y = 60, z = 684 + map_len/2}
-local m = peripheral.wrap("right")
-local old_term = term.redirect(m)
-local width, height = m.getSize()
-local cal_x = width/map_len
-local cal_z = height/map_len
+rednet.open("back")                 -- open rednet
+local m = peripheral.wrap("right")  -- connect to the "side" minitor
+local old_term = term.redirect(m)   -- redirect term to a screen
+
+-- need to be set
+local center_origin = {x = 130, y = 60, z = 684}    -- the middle of the base
+local map_len = 80                                  -- square base size in x and z
+
+-- variable
+local layer = {y_min = 0, y_max = 0}                                                                                    -- floor layer between y_min and y_max
+local data_phones = {{0,{x = 0, y = 0, z = 0}}}                                                                         -- empty load juste for autocomplete
+local data_rooms = {{x = 0, y = 0, z = 0, autorized = nil, receiver_id = 0, name = "", lpos = {x = 0, y = 0, z = 0}}}   -- empty load juste for autocomplete
+local data = {phones = data_phones, rooms = data_rooms}                                                                 -- empty load juste for autocomplete
+local map_origin = {x = 130 + map_len/2, y = 60, z = 684 + map_len/2}                                                   -- the bigger size of your base ( corner left of the screen ) 
+local width, height = m.getSize()                                                                                       -- window size
+local cal_x = width/map_len                                                                                             -- ratio size in x
+local cal_z = height/map_len                                                                                            -- ratio size in z
+
+-- set window
 m.setCursorPos(1, 1)
 m.setTextScale(1)
 m.setTextColor(colors.orange)
 
+
+-- function
 function is_in_area(playerPos, rectCorner1, rectCorner2)
     return
         playerPos.x >= math.min(rectCorner1.x, rectCorner2.x) and
@@ -50,6 +58,7 @@ function get_data()
     end
 end
 
+-- main loop
 while true do
     get_data()
     clear_window()
